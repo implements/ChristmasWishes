cc65ab4231517f90f3028cce44dca3395447cefc
Thoai-Lam Nguyen <implements@users.noreply.github.com>
2016-11-17 23:26:27 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,81 @@
+angular.module('ChristmasApplication', []).controller('ChristmasController', function($scope) {	
+	$scope.wishes = [
+		{
+			brand: "Apple",
+			title: "USB-C til Lightning-kabel",
+			url: "http://www.apple.com/dk/shop/product/MK0X2ZM/A/usb-c-til-lightning-kabel-1-m",
+			details: "1 meter",
+			price: "159 kr."
+		},
+		{
+			brand: "Apple",
+			title: "USB-C Strømforsyning",
+			url: "http://www.apple.com/dk/shop/product/MNF72Z/A/apple-usb-c-str%C3%B8mforsyning-p%C3%A5-61-w",
+			details: "61 watt",
+			price: "579 kr."
+		},
+		{
+			brand: "Logitech",
+			title: "G502 Proteus Spectrum",
+			url: "http://www.pricerunner.dk/pl/110-3141438/Mus/Logitech-G502-Proteus-Spectrum-Sammenlign-Priser",
+			details: "",
+			price: "599 kr."
+		},
+		{
+			brand: "Smith",
+			title: "Maze",
+			url: "http://www.smithoptics.com/us/Root/Men%27s/Helmets/Snow/Maze/p/H17-MZHFLGMIPS/sizeVariants",
+			details: "Matte Black - Medium",
+			price: "799 kr."
+		},
+		{
+			brand: "Joli Originals",
+			title: "Smooth Macbook Pro (Late 2016) Sleeve",
+			url: "https://jolioriginals.com/collections/smooth/products/smooth-macbook",
+			details: "Grey Leather & Red Lining",
+			price: "849 kr."
+		},
+		{
+			brand: "Nike",
+			title: "Free RN Distance",
+			url: "http://store.nike.com/dk/en_gb/pd/free-rn-distance-running-shoe/pid-11054016/pgid-11161865",
+			details: "Orange - Størrelse 43",
+			price: "1.049 kr."
+		},
+		{
+			brand: "Apple",
+			title: "TV",
+			url: "http://www.pricerunner.dk/pl/649-3098809/Medieafspillere/Apple-TV-32GB-%284th-Gen%29-Sammenlign-Priser",
+			details: "32 GB - 4th Generation",
+			price: "1.199 kr."
+		},
+		{
+			brand: "Nikon",
+			title: "AF-S DX NIKKOR",
+			url: "http://www.pricerunner.dk/pl/543-1209527/Kamera-Objektiver/Nikon-AF-S-DX-NIKKOR-35mm-F-1.8G-Sammenlign-Priser",
+			details: "35mm F/1.8G",
+			price: "1.395 kr."
+		},
+		{
+			brand: "Apple",
+			title: "Designed by Apple in California",
+			url: "http://www.apple.com/designed-by-apple/",
+			details: "Small",
+			price: "1.499 kr."
+		},
+		{
+			brand: "Sony",
+			title: "Playstation 4 Pro",
+			url: "http://www.pricerunner.dk/pl/52-3264582/Spillekonsoller/Sony-Playstation-4-Pro-1TB-%28PS4%29-Sammenlign-Priser",
+			details: "1 TB",
+			price: "3.099 kr."
+		},
+		{
+			brand: "Sony",
+			title: "Playstation VR",
+			url: "http://www.pricerunner.dk/pl/1469-3161919/VR-Virtual-reality/Sony-Playstation-VR-Sammenlign-Priser",
+			details: "",
+			price: "3.699 kr."
+		}
+	]
+});
