a8f6d11c4c3416948ecae8a4592c6672d6c69709
Thoai-Lam Nguyen <thoailam@gmail.com>
2021-10-15 08:38:57 +0200

    Added 2021.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 angular.module('ChristmasApplication', []).controller('ChristmasController', function($http, $scope) {	
 	
-	$http.get('./years/2020.json').success(function(response) {
+	$http.get('./years/2021.json').success(function(response) {
 		$scope.wishes = response;
 	});
 });
